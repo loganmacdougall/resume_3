4ad15fff942db7a4e9a5655b1001e968c398e8f4
Logan MacDougall <loganmac3000@hotmail.com>
2024-07-09 13:48:35 -0300

    Added jiggle text to projects page

--- projects.php
+++ projects.php
@@ -6,6 +6,16 @@
 </head>
 <body>
     <?php include("./header.php"); ?>
-    
+    <h1 id="title_name">
+            <span class="jiggleable">P</span> <!--
+        --> <span class="jiggleable">r</span> <!--
+        --> <span class="jiggleable">o</span> <!--
+        --> <span class="jiggleable">j</span> <!--
+        --> <span class="jiggleable">e</span> <!--
+        --> <span class="jiggleable">c</span> <!--
+        --> <span class="jiggleable">t</span> <!--
+        --> <span class="jiggleable">s</span>
+    </h1>
+    <script src="./script.js"></script>
 </body>
 </html>
