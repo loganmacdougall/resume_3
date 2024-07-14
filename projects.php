c0719a058059a65e9cf68c72a3f3e6a4b512c6a7
Logan MacDougall <lg463300@dal.ca>
2024-07-14 10:13:55 -0300

    Project page is complete

--- projects.php
+++ projects.php
@@ -22,75 +22,86 @@
         <div class="grid-division">
             <p>Web</p>
         </div>
-        <a href="https://loganmacdougall.com/madlibs-to-C" class="card-header">
-            <p>Madlibs-to-C</p>
-            <p>Create a madlibs story and turn it into a C Source file</p>
+        <a href="https://loganmacdougall.com/madlibs-to-C" style="background: url(./media/madlibs-to-C.png);" class="project_card">
+            <div class="card-header">
+                <p>Madlibs-to-C</p>
+                <p>Create a madlibs story and turn it into a C Source file</p>
+            </div>
         </a>
-        <a href="https://loganmacdougall.com/quickpads" class="card-header">
-            <p>Quickpads</p>
-            <p>An online notepad app designed for convenience</p>
+        <a href="https://loganmacdougall.com/quickpads" style="background: #555555 url(./media/quickpads.png);background-position: bottom;background-repeat:no-repeat" class="project_card">
+            <div class="card-header">
+                <p>Quickpads</p>
+                <p>An online notepad app designed for convenience</p>
+            </div>
         </a>
-        <a href="https://loganmacdougall.com/binary/" class="card-header">
-            <p>Binary</p>
-            <p>binary, decimal, and hex conversions and float to IEEE bits conversions</p>
+        <a href="https://loganmacdougall.com/binary" style="background: url(./media/binary.png);background-position: bottom;" class="project_card">
+            <div class="card-header">
+                <p>Binary</p>
+                <p>binary, decimal, and hex conversions and float to IEEE bits conversions</p>
+            </div>
         </a>
-        <a href="https://loganmacdougall.com/star-a-thon" class="card-header">
-            <p>Star-A-Thon</p>
-            <p>An extra challenge while playing Mario64</p>
+        <a href="https://loganmacdougall.com/star-a-thon" style="background: url(./media/star-a-thon.png);" class="project_card">
+            <div class="card-header">
+                <p>Star-A-Thon</p>
+                <p>An extra challenge while playing Mario64</p>
+            </div>
         </a>
-        <a href="https://loganmacdougall.com/symbols/" class="card-header">
-            <p>Symbols</p>
-            <p>A website for copying common symbols</p>
+        <a href="https://loganmacdougall.com/symbols" style="background: url(./media/symbols.png);background-position:bottom;" class="project_card">
+            <div class="card-header">
+                <p>Symbols</p>
+                <p>A website for copying common symbols</p>
+            </div>
         </a>
-        <a href="https://loganmacdougall.com/robot-simon-says/" class="card-header">
-            <p>Robot Simon Says</p>
-            <p>An children's education website for teaching the basics of programming</p>
+        <a href="https://loganmacdougall.com/robot-simon-says" style="background: #1e1e1e url(./media/robot-simon-says.png);background-position:bottom;background-repeat:no-repeat;" class="project_card">
+            <div class="card-header">
+                <p>Robot Simon Says</p>
+                <p>An children's education website for teaching the basics of programming</p>
+            </div>
         </a>
-        <a href="https://loganmacdougall.com/ultimate-tic-tac-toe/" class="card-header">
-            <p>Ultimate Tic-Tac-Toe</p>
-            <p>An implementation of the game "Ultimate Tic-Tak-Toe</p>
-        </a>
-        <div class="grid-division">
-            <p>Discord Bots</p>
-        </div>
-        <a href="./" class="card-header">
-            <p>Brute Bot</p>
-            <p>Performs brute force attacks fully controlled within discord</p>
-        </a>
-        <a href="./" class="card-header">
-            <p>Minecraft Player Tracker</p>
-            <p>Keeps record of the times players leave and join a minecraft server and alerts the activity of specified players</p>
-        </a>
-        <a href="./" class="card-header">
-            <p>ToonTown Invasion Tracker</p>
-            <p>Alerts me when specified invasion events start and end</p>
+        <a href="https://loganmacdougall.com/ultimate-tic-tac-toe" style="background: url(./media/ultimate-tic-tac-toe.png);background-position:center;" class="project_card">
+            <div class="card-header">
+                <p>Ultimate Tic-Tac-Toe</p>
+                <p>An implementation of the game "Ultimate Tic-Tak-Toe</p>
+            </div>
         </a>
         <div class="grid-division">
             <p>Applications</p>
         </div>
-        <a href="https://github.com/loganmacdougall/Word-Finder-Solver" class="card-header">
-            <p>Word Finder Solver</p>
-            <p>A program for solving "Word Finder" type games</p>
+        <a href="https://github.com/loganmacdougall/BruteBot" style="background: #ececec url(./media/brute-bot.png);background-position:bottom;background-repeat: no-repeat;" class="project_card">
+            <div class="card-header">
+                <p>Brute Bot</p>
+                <p>Performs brute force attacks fully controlled within discord</p>
+            </div>
         </a>
-        <a href="./" class="card-header">
-            <p>Flight Simulator</p>
-            <p>A flight simulator written in C</p>
+        <a href="https://github.com/loganmacdougall/Word-Finder-Solver" style="background: #181818 url(./media/word-finder-solver.png);background-position:bottom left;background-repeat:no-repeat;" class="project_card">
+            <div class="card-header">
+                <p>Word Finder Solver</p>
+                <p>A program for solving "Word Finder" type games</p>
+            </div>
         </a>
-        <a href="./" class="card-header">
-            <p>C mini projects</p>
-            <p>Several C projects which progressively get more challenging</p>
+        <a href="https://github.com/loganmacdougall/Flight-Simulator" style="background: url(./media/flight-simulator.png);background-position:bottom;" class="project_card">
+            <div class="card-header">
+                <p>Flight Simulator</p>
+                <p>A flight simulator written in C and OpenGL</p>
+            </div>
         </a>
-        <a href="./" class="card-header">
-            <p>StringLGP</p>
-            <p>A genetic algorithm to solve password vault challenges</p>
+        <a href="https://github.com/loganmacdougall/C-mini-projects" style="background:url(./media/C-mini-projects.png)" class="project_card">
+            <div class="card-header">
+                <p>C Mini Projects</p>
+                <p>Several C projects which progressively get more challenging</p>
+            </div>
         </a>
-        <a href="./" class="card-header">
-            <p>StarFight</p>
-            <p>A 2-player turn-based space shooter</p>
+        <a href="https://github.com/loganmacdougall/StringGP" style="background: url(./media/stringgp.png);background-position:bottom;" class="project_card">
+            <div class="card-header">
+                <p>StringLGP</p>
+                <p>A genetic algorithm to solve password vault challenges</p>
+            </div>
         </a>
-        <a href="./" class="card-header">
-            <p>Gunned Down</p>
-            <p>A puzzle game to track down a ship with delayed limited information</p>
+        <a href="https://github.com/loganmacdougall/StarFight" style="background: #00001e url(./media/starfight.png);background-position:center;background-repeat:no-repeat;" class="project_card">
+            <div class="card-header">
+                <p>StarFight</p>
+                <p>A 2-player turn-based space shooter</p>
+            </div>
         </a>
     </section>
     <script src="./script.js"></script>
