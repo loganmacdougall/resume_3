6d842e2f8c63e1069b0eaf80ff9cad504448ef34
Logan MacDougall <lg463300@dal.ca>
2024-07-15 17:19:06 -0300

    Fixed PHP version conflict error

--- check-for-redirects.php
+++ check-for-redirects.php
@@ -1,5 +1,16 @@
 <?php
 
+if (!function_exists('str_ends_with')) {
+  function str_ends_with($haystack, $needle)
+  {
+    $strlen_needle = mb_strlen($needle);
+    if (mb_substr($haystack, -$strlen_needle, $strlen_needle) == $needle) {
+      return true;
+    }
+    return false;
+  }
+}
+
 $uri = $_SERVER['REQUEST_URI'];
 
 if (str_ends_with($uri, "/projects")) {
